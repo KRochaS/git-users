a28f91f7e14658276c6faafc2f74a32fa075e346
KRochaS <karinerocha.s@hotmail.com>
2024-02-02 19:05:00 -0300

    feat: card component

--- modules/shared/ui/src/lib/shared-ui/components/card/card.component.ts
+++ modules/shared/ui/src/lib/shared-ui/components/card/card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'git-users-card',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.scss',
+    selector: 'git-users-card',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.scss',
 })
-export class CardComponent {}
+export class CardComponent {
+
+    @Input() name!: string | undefined;
+    @Input() avatarUrl!: string | undefined;
+    @Input() url!: string | undefined;
+    @Input() bio!: string | undefined;
+    @Input() login!: string | undefined;
+    @Input() followers!: number | undefined;
+
+
+}
