7e40d3ab88f4df165434dba8b1defd8e7c7ffe3f
KRochaS <karinerocha.s@hotmail.com>
2024-02-02 19:13:09 -0300

    chore: Add new exports components

--- modules/shared/ui/src/index.ts
+++ modules/shared/ui/src/index.ts
@@ -1,3 +1,7 @@
+export * from './lib/shared-ui/components/card-list/card-list.component';
 export * from './lib/shared-ui/components/card/card.component';
 export * from './lib/shared-ui/components/header/header.component';
+export * from './lib/shared-ui/components/input/input.component';
+export * from './lib/shared-ui/components/loading/loading.interceptor';
+export * from './lib/shared-ui/components/loading/loading.module';
 
