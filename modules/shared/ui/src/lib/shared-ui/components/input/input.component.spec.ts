6fc1f594714e9769ba7a76e99680e765d599bd69
KRochaS <karinerocha.s@hotmail.com>
2024-02-07 16:38:10 -0300

    feat: Input Unit Tests

--- modules/shared/ui/src/lib/shared-ui/components/input/input.component.spec.ts
+++ modules/shared/ui/src/lib/shared-ui/components/input/input.component.spec.ts
@@ -1,21 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input.component';
 
 describe('InputComponent', () => {
-  let component: InputComponent;
-  let fixture: ComponentFixture<InputComponent>;
+    let component: InputComponent;
+    let fixture: ComponentFixture<InputComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [InputComponent],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [InputComponent, ReactiveFormsModule,],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(InputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(InputComponent);
+        component = fixture.componentInstance;
+        component.control = new FormControl();
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render input element with specified type', () => {
+        component.type = 'search';
+        fixture.detectChanges();
+        const inputElement: HTMLInputElement = fixture.nativeElement.querySelector('input');
+        expect(inputElement).toBeTruthy();
+        expect(inputElement.type).toBe(component.type);
+    });
+
+    it('should set placeholder attribute', () => {
+        const placeholder = 'Buscar usuários';
+        component.placeholder = placeholder;
+        fixture.detectChanges();
+        const inputElement: HTMLInputElement = fixture.nativeElement.querySelector('input');
+        expect(inputElement.getAttribute('placeholder')).toBe(component.placeholder);
+        expect(component.placeholder).toBe(placeholder);
+    });
+
+    it('should bind to form control', () => {
+        const inputElement: HTMLInputElement = fixture.nativeElement.querySelector('input');
+        expect(component.control.value).toBe(null);
+        inputElement.value = 'Test Value';
+        inputElement.dispatchEvent(new Event('input'));
+        expect(component.control.value).toBe('Test Value');
+    });
 });
